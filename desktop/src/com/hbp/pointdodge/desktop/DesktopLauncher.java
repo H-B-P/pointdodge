9ab0845538019e594a52997bd9f0d4f15499af34
H-B-P <pointy@protonmail.ch>
2016-08-19 11:30:20 +0100

    Initial commit of pointdodge.

---
+++ desktop/src/com/hbp/pointdodge/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.hbp.pointdodge.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hbp.pointdodge.PointDodge;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PointDodge(), config);
+	}
+}
---
+++ html/src/com/hbp/pointdodge/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.hbp.pointdodge.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.hbp.pointdodge.PointDodge;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new PointDodge();
+        }
+}
